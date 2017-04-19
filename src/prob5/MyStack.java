06c9614594344a64c36cbcc643d6ba15d68735ea
bit-user <bit-user@bit>
2017-04-19 17:46:39 +0900

    .

--- src/prob5/MyStack.java
+++ src/prob5/MyStack.java
@@ -3,16 +3,22 @@ package prob5;
 public class MyStack<T> {
 	int top;
 	String[] buffer;
-	MyStack(){}
+	//private int inputLength;
+	MyStack(){
+		this.top=0;
+		this.buffer = new String[10];
+		//this.inputLength=10;
+	}
 	
 	MyStack(int value){
 		this.top=0;
 		this.buffer = new String[value];
+		//this.inputLength=value;
 	}
 	
 	public void push(String string) {
 		
-		if(top>=buffer.length-1){
+		if(top>=buffer.length-2){
 			resize(); 
 		}else{
 			this.buffer[top++] = string;
@@ -37,7 +43,7 @@ public class MyStack<T> {
 		for(int i = 0 ; i <this.buffer.length ; ++i){
 			tempBuff[i] = this.buffer[i];
 		}
-		this.buffer = new String[buffer.length*2];
+		this.buffer = new String[tempBuff.length*2];
 		for(int i = 0 ; i <tempBuff.length ; ++i){
 			this.buffer[i] = tempBuff[i];
 		}
